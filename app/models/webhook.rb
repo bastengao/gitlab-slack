bad4bb91ede90172f0a0892d7710ee2401db1c1b
bastengao <bastengao@gmail.com>
2015-02-28 20:18:49 +0800

    ehance show page

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -2,6 +2,9 @@ class Webhook < ActiveRecord::Base
   belongs_to :user
 
   validates :name, :slack_incoming_hook, presence: true
+  validates :slack_incoming_hook, format: { with: /https:\/\/hooks.slack.com\/services\/+/}
 
-  # TODO format
+  def hook_id
+    ScatterSwap.hash(id)
+  end
 end
